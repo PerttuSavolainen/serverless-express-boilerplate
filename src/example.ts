c856b93ddf24912e330eb5e2b886dd63d072a34c
Perttu Savolainen <savolainen.perttu@gmail.com>
2019-05-26 11:52:25 +0300

    Add xray tracing plugin and packages

--- src/example.ts
+++ src/example.ts
@@ -1,5 +1,8 @@
+// Import aws modules with XRay tracing capabilities
+// import * as awsXRay from 'aws-xray-sdk';
+// const { Lambda } = awsXRay.captureAWS(require('aws-sdk'));
+
 const exampleLambda = async (req: any, res: any) => {
-  console.log("This is an example lambda");
   res.send("Hello there!");
 };
 
